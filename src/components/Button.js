4bcca7c084055b3e084397de83b682f018ce5a8b
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2023-07-30 15:32:13 +0200

    Simplifying the Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,14 @@
 export default function Button({ title, onClick, splashscreen = false }) {
-  if (splashscreen) {
-    return (
-      <button
-        onClick={onClick}
-        className="w-400 h-50 my-1 rounded-full border-4 border-black bg-black text-2xl text-white font-medium hover:text-black hover:bg-white active:bg-gray active:text-black transition-all duration-150"
-      >
-        <p>{title}</p>
-      </button>
-    );
-  } else {
-    return (
-      <button
-        onClick={onClick}
-        className="w-auto h-50 my-1 px-8 rounded-full border-4 border-black bg-black text-lg text-white font-medium hover:text-black hover:bg-white active:bg-gray active:text-black transition-all duration-150"
-      >
-        <p>{title}</p>
-      </button>
-    );
-  }
+  return (
+    <button
+      onClick={onClick}
+      className={`${
+        splashscreen ? "w-400" : "w-auto px-8"
+      } h-50 my-1 rounded-full border-4 border-black bg-black text-2xl text-white font-medium hover:text-black hover:bg-white active:bg-gray active:text-black transition-all duration-150 ${
+        splashscreen ? "w-400" : "w-auto px-8"
+      } `}
+    >
+      <p>{title}</p>
+    </button>
+  );
 }
