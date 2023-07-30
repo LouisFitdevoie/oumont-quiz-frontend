bac7c425442c11c6d9dc9f04b9dba0c6b15239b3
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2023-07-30 10:31:15 +0200

    Button component updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,21 @@
-export default function Button({ title, onClick }) {
-  return (
-    <button
-      onClick={onClick}
-      className="w-1/6 p-2 my-1 rounded-full border-4 border-black bg-black text-white font-medium hover:text-black hover:bg-white active:bg-gray active:text-black transition-all duration-150"
-    >
-      <p>{title}</p>
-    </button>
-  );
+export default function Button({ title, onClick, splashscreen = false }) {
+  if (splashscreen) {
+    return (
+      <button
+        onClick={onClick}
+        className="w-400 h-50 my-1 rounded-full border-4 border-black bg-black text-2xl text-white font-medium hover:text-black hover:bg-white active:bg-gray active:text-black transition-all duration-150"
+      >
+        <p>{title}</p>
+      </button>
+    );
+  } else {
+    return (
+      <button
+        onClick={onClick}
+        className="w-auto h-50 my-1 px-8 rounded-full border-4 border-black bg-black text-lg text-white font-medium hover:text-black hover:bg-white active:bg-gray active:text-black transition-all duration-150"
+      >
+        <p>{title}</p>
+      </button>
+    );
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      black: "#1e1e1e",
-      gray: "#dfdfdf",
-      green: "#6ade7d",
-      red: "#f46546",
-      white: "#f0f0f0",
-      yellow: "#f4c546",
+    extend: {
+      colors: {
+        black: "#1e1e1e",
+        gray: "#dfdfdf",
+        green: "#6ade7d",
+        red: "#f46546",
+        white: "#f0f0f0",
+        yellow: "#f4c546",
+      },
+      spacing: {
+        40: "40px",
+        50: "50px",
+        400: "400px",
+      },
     },
   },
   plugins: [],
