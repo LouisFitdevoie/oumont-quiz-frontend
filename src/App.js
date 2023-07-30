2f29609e05157a2dbe29512843814713cab58604
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2023-07-30 18:52:37 +0200

    Starting the navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="bg-yellow w-screen h-screen">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-game" element={<h1>Create Game</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
