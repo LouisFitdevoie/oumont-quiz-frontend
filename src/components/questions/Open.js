ccfff2ec8c14e63f92045ce5434c31638371ea80
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2023-08-06 22:09:00 +0200

    Adding component + updating MultipleChoice & QuestionButtons

---
+++ src/components/questions/Open.js
@@ -0,0 +1,20 @@
+export default function Open({
+  question,
+  correctAnswer,
+  timeToAnswer,
+  isAnswerShown,
+}) {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="w-5/6 bg-white border-2 border-black rounded-2xl text-center font-medium py-2 px-4">
+        <h1 className="text-2xl font-bold">Question ouverte</h1>
+        <p className="text-xl">{question}</p>
+      </div>
+      {isAnswerShown && (
+        <div className="mt-5 w-5/6 bg-green border-2 border-black rounded-2xl text-center font-medium py-2 px-4">
+          <u className="font-semibold">Réponse correcte :</u> {correctAnswer}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/questions/Question.js
+++ src/components/questions/Question.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 import MultipleChoice from "./MultipleChoice";
 import QuestionButtons from "./QuestionButtons";
+import Open from "./Open";
 
 export default function App({ question, handleNextQuestion, questionNumber }) {
   const [isAnswerShown, setIsAnswerShown] = useState(false);
 
   //TODO : add timer and hide buttons before timer ends
+  //TODO : add explanation
 
   if (question.questionType === "multipleChoice") {
     return (
@@ -27,7 +29,22 @@ export default function App({ question, handleNextQuestion, questionNumber }) {
       </div>
     );
   } else if (question.questionType === "open") {
-    return <p>OPEN</p>;
+    return (
+      <div className="w-full h-full flex flex-col justify-between">
+        <Open
+          question={question.question}
+          correctAnswer={question.answer}
+          timeToAnswer={10}
+          isAnswerShown={isAnswerShown}
+        />
+        <QuestionButtons
+          handleNextQuestion={handleNextQuestion}
+          isAnswerShown={isAnswerShown}
+          questionNumber={questionNumber}
+          setIsAnswerShown={setIsAnswerShown}
+        />
+      </div>
+    );
   } else if (question.questionType === "estimate") {
     return <p>ESTIMATE</p>;
   }
