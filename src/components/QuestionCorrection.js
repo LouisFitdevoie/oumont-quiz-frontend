5659d12e9ab556369ded3cb92692fc6b98c384b2
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2023-08-16 16:03:56 +0200

    Adding ability to add half the points of an open/estimate question

--- src/components/QuestionCorrection.js
+++ src/components/QuestionCorrection.js
@@ -11,29 +11,76 @@ export default function QuestionCorrection({
   currentGroup,
 }) {
   const [isCorrectClicked, setIsCorrectClicked] = useState(false);
+  const [isAddHalfPointsClicked, setIsAddHalfPointsClicked] = useState(false);
   const [isIncorrectClicked, setIsIncorrectClicked] = useState(false);
 
   const handleCorrectClick = () => {
-    if (isCorrectClicked) return;
-    if (isIncorrectClicked) {
-      setIsCorrectClicked(true);
-      setIsIncorrectClicked(false);
-      setPoints(points + question.points);
+    if (question.questionType === "multipleChoice") {
+      if (isCorrectClicked) return;
+      if (isIncorrectClicked) {
+        setIsCorrectClicked(true);
+        setIsIncorrectClicked(false);
+        setPoints(points + question.points);
+        return;
+      }
+    } else {
+      if (isCorrectClicked) return;
+      if (isIncorrectClicked) {
+        setIsCorrectClicked(true);
+        setIsIncorrectClicked(false);
+        setPoints(points + question.points);
+        return;
+      }
+      if (isAddHalfPointsClicked) {
+        setIsCorrectClicked(true);
+        setIsAddHalfPointsClicked(false);
+        setPoints(points + question.points / 2);
+        return;
+      }
+    }
+  };
+
+  const handleAddHalfPoints = () => {
+    if (question.questionType === "multipleChoice") return;
+    if (isAddHalfPointsClicked) return;
+    if (isCorrectClicked) {
+      setIsCorrectClicked(false);
+      setIsAddHalfPointsClicked(true);
+      setPoints(points - question.points / 2);
       return;
     }
-    setIsCorrectClicked(true);
-    setPoints(points + question.points);
+    if (isIncorrectClicked) {
+      setIsIncorrectClicked(false);
+      setIsAddHalfPointsClicked(true);
+      setPoints(points + question.points / 2);
+      return;
+    }
   };
 
   const handleIncorrectClick = () => {
-    if (isIncorrectClicked) return;
-    if (isCorrectClicked) {
-      setIsIncorrectClicked(true);
-      setIsCorrectClicked(false);
-      setPoints(points - question.points);
-      return;
+    if (question.questionType === "multipleChoice") {
+      if (isIncorrectClicked) return;
+      if (isCorrectClicked) {
+        setIsCorrectClicked(false);
+        setIsIncorrectClicked(true);
+        setPoints(points - question.points);
+        return;
+      }
+    } else {
+      if (isIncorrectClicked) return;
+      if (isCorrectClicked) {
+        setIsCorrectClicked(false);
+        setIsIncorrectClicked(true);
+        setPoints(points - question.points);
+        return;
+      }
+      if (isAddHalfPointsClicked) {
+        setIsAddHalfPointsClicked(false);
+        setIsIncorrectClicked(true);
+        setPoints(points - question.points / 2);
+        return;
+      }
     }
-    setIsIncorrectClicked(true);
   };
 
   const possibleAnswers = ["A", "B", "C", "D"];
@@ -76,6 +123,20 @@ export default function QuestionCorrection({
         >
           <DoneIcon className="h-4 w-4 self-center" />
         </button>
+        {question.questionType !== "multipleChoice" && (
+          <button
+            onClick={() => handleAddHalfPoints()}
+            className={`h-8 w-8 ml-2 flex items-center justify-center border rounded-full text-white transition-all duration-100 ${
+              isAddHalfPointsClicked
+                ? "border-orange bg-orange"
+                : "border-black bg-black"
+            } hover:bg-white hover:text-black hover:border-black active:bg-gray active:text-black
+                `}
+            title="Ajouter la moitié des points"
+          >
+            <p className="self-center">+/-</p>
+          </button>
+        )}
         <button
           onClick={() => handleIncorrectClick()}
           className={`h-8 w-8 ml-2 flex items-center justify-center border rounded-full text-white transition-all duration-100 ${
