2d1773a5188728ffdfdcacee06d2a51425b8b426
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2025-09-10 17:25:07 +0200

    Some updates

--- src/components/ui/QuestionDifficultyLevel.js
+++ src/components/ui/QuestionDifficultyLevel.js
@@ -2,7 +2,7 @@ export default function QuestionDifficultyLevel({ points }) {
   if (points === 1) {
     return (
       <div className="flex flex-row items-center ml-3">
-        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-green">
+        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-green font-black">
           <p>Facile</p>
         </div>
       </div>
@@ -10,7 +10,7 @@ export default function QuestionDifficultyLevel({ points }) {
   } else if (points === 2) {
     return (
       <div className="flex flex-row items-center ml-3">
-        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-yellow">
+        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-yellow font-black">
           <p>Moyen</p>
         </div>
       </div>
@@ -18,7 +18,7 @@ export default function QuestionDifficultyLevel({ points }) {
   } else if (points === 3) {
     return (
       <div className="flex flex-row items-center ml-3">
-        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-red">
+        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-red font-black">
           <p>Difficile</p>
         </div>
       </div>
