0b8fafd4c9d62d08c6655568b996781a4b859cb0
LouisFitdevoie <65094430+LouisFitdevoie@users.noreply.github.com>
2026-08-30 11:13:49 +0200

    Updates for readability

--- src/components/ui/QuestionDifficultyLevel.js
+++ src/components/ui/QuestionDifficultyLevel.js
@@ -2,7 +2,7 @@ export default function QuestionDifficultyLevel({ points }) {
   if (points === 1) {
     return (
       <div className="flex flex-row items-center ml-3">
-        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-green font-black">
+        <div className="rounded-lg mr-2 px-2 py-1 text-black bg-green font-black">
           <p>Facile</p>
         </div>
       </div>
@@ -10,7 +10,7 @@ export default function QuestionDifficultyLevel({ points }) {
   } else if (points === 2) {
     return (
       <div className="flex flex-row items-center ml-3">
-        <div className="rounded-lg mr-2 px-2 py-1 text-white bg-yellow font-black">
+        <div className="rounded-lg mr-2 px-2 py-1 text-black bg-yellow font-black">
           <p>Moyen</p>
         </div>
       </div>
